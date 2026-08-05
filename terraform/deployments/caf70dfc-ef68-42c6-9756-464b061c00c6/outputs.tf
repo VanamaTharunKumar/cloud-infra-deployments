d2581a173b5e22907a284103034ffe0d2c590e5c
Tharun <tharunkumarvanama@gmail.com>
2026-08-05 10:49:49 +0000

    Generated deployment 79cd5508-7dfe-4ec7-bd24-28b619215bd3

--- terraform/deployments/caf70dfc-ef68-42c6-9756-464b061c00c6/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  value       = aws_instance.app_server.public_ip
-  description = "The public IP address of the EC2 instance"
-}
-
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "The ID of the created VPC"
-}
